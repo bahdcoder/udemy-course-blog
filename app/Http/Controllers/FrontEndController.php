a75f62e8a147ab8242d2dbd176a025ea23600719
kati-frantz <frantzenterprises777@gmail.com>
2017-01-17 05:38:09 +0400

    begin single page

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -16,6 +16,9 @@ class FrontEndController extends Controller
                 ->with('categories', Category::take(5)->get())
                 ->with('first_post', Post::orderBy('created_at', 'desc')->first())
                 ->with('second_post', Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
-                ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first());
+                ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first())
+                ->with('career', Category::find(7))
+                ->with('tutorials', Category::find(6))
+                ->with('settings', Setting::first());
     }
 }
